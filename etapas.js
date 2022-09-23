4480fd8304032fdea2211a6b84530c101a1c14f6
Fernando <111437701+FernandoKrunts@users.noreply.github.com>
2022-09-23 13:25:05 -0300

    versão1

---
+++ etapas.js
@@ -0,0 +1,57 @@
+let etapas =[
+    {
+        titulo:"VEREADOR",
+        numeros:'5',
+        candidados:[
+            {
+                numero:'38111',
+                nome:"Fulando de tal",
+                partido: "ABC" ,
+                fotos:[
+                    {
+                        url:'lula.jpg' , legenda : 'Vereador' ,
+                    }
+                ]
+            
+            },
+            {
+                numero:'77222',
+                nome:"Beltrano da silva",
+                partido: "DEFG" ,
+                fotos:[
+                    {
+                        url:'bolsonaro.jpg' , legenda : 'Vereador' , 
+                    }
+                ]
+                
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: '2' ,
+        candidados :[
+            {
+                numero: '12',
+                nome: 'Ciro',
+                partido : 'ABC',
+                vice: 'Sei lá',
+                fotos:[
+                    {url:'ciro.jpg' , legenda: 'Presidente'},
+                    {url:'naoesi.jpg' , legenda: 'Vice-Presidente', small:true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Dilma',
+                partido : 'ABC',
+                vice: 'Mourão',
+                fotos:[
+                    {url:'dilma.jpg' , legenda: 'Presidente'},
+                    {url:'mourao.jpg' , legenda: 'Vice-Presidente', small:true}
+                ]
+
+            }
+        ]
+    }
+]
